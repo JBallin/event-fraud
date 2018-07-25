5da0cdbfedf8cffcd880d4f0b3a30212725f5469
JBallin <JBallin@users.noreply.github.com>
2018-07-25 13:36:33 -0700

    Add container
    Co-Authored-By: hyperrex <mrwendell@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ToolBar from './components/ToolBar'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <ToolBar />
       </div>
     );
