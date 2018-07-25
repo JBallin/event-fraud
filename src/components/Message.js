74e97b15f02fef23303c5a1567edc9e936d9b698
JBallin <JBallin@users.noreply.github.com>
2018-07-25 15:32:24 -0700

    Return jsx in Message

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,19 +4,21 @@ const isRead = message => message.read ? 'read' : 'unread';
 const isSelected = message => message.selected ? 'selected' : '';
 
 export default ({ message }) => {
-  <div className={`row message ${isRead(message)} ${isSelected(message)}`}>
-    <div className="col-xs-1">
-      <div className="row">
-        <div className="col-xs-2">
-          <input type="checkbox" />
-        </div>
-        <div className="col-xs-2">
-          <i className="star fa fa-star-o"></i>
+  return(
+    <div className={`row message ${isRead(message)} ${isSelected(message)}`}>
+      <div className="col-xs-1">
+        <div className="row">
+          <div className="col-xs-2">
+            <input type="checkbox" />
+          </div>
+          <div className="col-xs-2">
+            <i className="star fa fa-star-o"></i>
+          </div>
         </div>
       </div>
+      <div className="col-xs-11">
+        <a href="#">{message.subject}</a>
+      </div>
     </div>
-    <div className="col-xs-11">
-      <a href="#">{message.subject}</a>
-    </div>
-  </div>
+  )
 }
