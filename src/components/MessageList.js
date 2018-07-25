a5d9279728e5a5df6683008c7528b9460037dc02
JBallin <JBallin@users.noreply.github.com>
2018-07-25 15:33:11 -0700

    Add key to Message

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,5 +2,5 @@ import React from 'react'
 import Message from './Message'
 
 export default ({ messages }) => {
-  return messages.map(message => <Message message={message} />)
+  return messages.map(message => <Message message={message} key={message.id} />)
 }
