14f4603826e0a5876089eea743f6cb5bab45dc6e
JBallin <JBallin@users.noreply.github.com>
2018-07-25 13:32:09 -0700

    Add ToolBar
    Co-Authored-By: hyperrex <mrwendell@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ToolBar from './components/ToolBar'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <ToolBar />
       </div>
     );
   }
